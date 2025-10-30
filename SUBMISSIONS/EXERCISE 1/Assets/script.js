02f7eb138970e187ccae01d452fb09453490e0c1
alevanella-boop <alevanella@gmail.com>
2025-10-30 09:46:51 +0100

    lesson-03

---
+++ SUBMISSIONS/EXERCISE 1/Assets/script.js
@@ -0,0 +1,18 @@
+//makes thing appear in the browser terminal
+//console console.log();
+
+// javascript works with different types of variables
+//string variable is a text variable, put in quotes
+//number variable is a number, no quotes
+//boolean variable is true or false
+
+//let (type of variable) something (name of variable) = "Hello World" (value of variable);
+//variables are only written in letters and numbers, camelCase
+
+//console.log(something)
+
+//constant are the opposite of variables, so the value cannot be changed
+//constants should be written in all caps?
+
+//var, another type of variable, older version of let
+
