cbbbb51597f6cbe53e668e85cc5aad733a9c26d2
alevanella-boop <alevanella@gmail.com>
2025-10-30 16:01:33 +0100

    css done and some javas intro

---
+++ Lessons/Lesson-03/Assets/script.js
@@ -0,0 +1,16 @@
+//create a constant in ordter to not have to write all the function everytime
+
+const addButton = document.getElementById('add-btn');
+
+//create a constant to actually add a task to the pin board once written into the input field
+const inputField = document.getElementById('task-input');
+
+// listener: when the button is clicked, run the function addTask
+//add a listener to the "add" button so the action can be executed
+addButton,addEventListener('click', () => {
+    //the way to detect if the button is actually being pressed pressed
+    //first javascript interaction with the html
+    console.log('Add button pressed!!!!')
+
+    
+});
